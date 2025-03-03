861a1e6880d4d8b7f79e91bf2b455fc5b1412dca
100454201 <100454201@alumnos.uc3m.es>
2025-03-03 12:24:23 +0100

    Version Final

--- main.py
+++ main.py
@@ -10,17 +10,25 @@ Funcionalidades:
 - Procesa cada línea y muestra los resultados en la consola.
 
 """
-
+import sys
 import re
 from lexer import construir_scanner
 from parser import construir_parser
 
+if len(sys.argv) != 2:
+    print("Uso: python main.py <archivo_entrada>")
+    sys.exit(1)
+input_file = sys.argv[1]
+
 lexer = construir_scanner()
 parser = construir_parser()
 
-
-with open("entrada.txt", "r") as f:
-    entrada = f.read()
+try:
+    with open(input_file, "r") as f:
+        entrada = f.read()
+except FileNotFoundError:
+    print(f"Error: No se encontro el archivo'{input_file}'")
+    sys.exit(1)
 
 entrada = re.sub(r"'''[\s\S]*?'''", "", entrada) 
 
